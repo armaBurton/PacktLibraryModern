96d06390681cccdb1e992403f7edaafab944ad19
Arma Burton <arma@armaburton.com>
2023-05-26 16:33:28 -0700

    updated

--- Book.cs
+++ Book.cs
@@ -1,8 +1,16 @@
 namespace Packt.Shared;
 
+
+
 public class Book{
   public required string? Isbn { get; set; }
   public required string? Title { get; set; }
   public string? Author { get; set; }
   public int PageCount { get; set; }
+
+  public Book(){}
+  public Book(string? isbn, string? title){
+    Isbn = isbn;
+    Title = title;
+  }
 }
